3ba0cb22a181b60449a82df6b71610bd3bcfda5c
Faza Thirafi <13514033@std.stei.itb.ac.id>
2021-01-28 14:04:45 +0900

    Add files via upload

---
+++ Java/Coba.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class Coba {
+    public static void main(String[] args) {
+        System.out.println("AAAAA");
+    }
+}
---
+++ Java/Try.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Try {
+
+	public static void main (String[] args) {
+
+	}
+}
